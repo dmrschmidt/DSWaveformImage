a6c4777f64645a67e9f2f1a4393e9761a42c6834
Ben Osheroff <ben@gimbo.net>
2020-01-11 13:11:37 -0800

    add package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DSWaveformImage",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "DSWaveformImage",
+            targets: ["DSWaveformImage"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "DSWaveformImage",
+            path: "DSWaveformImage"
+        )
+    ]
+)
